9bf688ea726994e80efb4449649768e37377ca77
EarTraining <68250652+EarTraining@users.noreply.github.com>
2020-10-07 16:37:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  
+//const [value, setValue] = useState(intialState);
+const [name, setName] = useState('');
+
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <h3>{name}</h3>
+      <input type="text" onChange={(e) => setName(e.target.value)} value={name}></input>
     </div>
   );
 };
