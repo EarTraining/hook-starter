c926896ca776a228824fedc0f898e613bfad110e
EarTraining <OKlinikowski@gmail.com>
2020-10-08 12:37:32 -0400

    changes from video 8: used useRef to edit DOM nodes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
 const App = () => {
   
+
 //const [value, setValue] = useState(intialState);
-/*  Moved for Video 7 **
 //const [name, setName] = useState('');
 
 //useEffect(() => {
   //updates actual tab title w/o interrupting lifecycle
   //document.title = name;
 //})
-*/
  
   const [name, setName] = useTitleInput('');
+  const ref = useRef();
+  //console.log('ref', ref.current);
 
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+    <div className="main-wrapper" ref={ref}>
+      <h1 onClick ={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
       <Toggle/>
       <form onSubmit={ e => {
           e.preventDefault();
