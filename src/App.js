5131c25bb0181660c949a4282c9186431cedf285
EarTraining <OKlinikowski@gmail.com>
2020-10-08 16:42:52 -0400

    Video 11 - using useMemo to determine when a fx should run

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef, createContext } from 'react';
+import React, { useState, useEffect, useRef, /*createContext,*/ useMemo } from 'react';
 import Toggle from './Toggle';
-import Counter from './Counter';
+//import Counter from './Counter';
 import { useTitleInput } from './hooks/useTitleInput';
 
 //export const UserContext = createContext();
@@ -20,15 +20,24 @@ const App = () => {
   const ref = useRef();
   //console.log('ref', ref.current);
 
+  const reverseWord = word => {
+    console.log('function called');
+    return word.split("").reverse().join("");
+  }
+
+  const title = 'Level Up Dishes';
+  const TitleReversed = useMemo(() => reverseWord(name), [name]);
+
   return (
     //<UserContext.Provider
       //value={{
         //user: true
       //}}>
     <div className="main-wrapper" ref={ref}>
-      <h1 onClick ={() => ref.current.classList.add('new-fake-class')}>Level Up Dishes</h1>
+      <h1 onClick ={() => ref.current.classList.add('new-fake-class')}>
+        {TitleReversed}
+      </h1>
       <Toggle/>
-      <Counter/>
       <form onSubmit={ e => {
           e.preventDefault();
           // ** FROM VIDEO 5 **
