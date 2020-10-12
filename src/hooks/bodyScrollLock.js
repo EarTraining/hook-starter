7d788bb7f175ecbbfe341aa7ceeed21048494ed7
EarTraining <OKlinikowski@gmail.com>
2020-10-12 13:37:21 -0400

    Video 17 - added way to toggle out of lockScroll state

--- src/hooks/bodyScrollLock.js
+++ src/hooks/bodyScrollLock.js
@@ -4,7 +4,12 @@ function useBodyScrollLock()
 {
     useLayoutEffect(() => {
         document.body.style.overflow = 'hidden';
-    });
+
+        //return function cleanup() {
+        return () => {
+            document.body.style.overflow = '';
+        };
+    }, []);
 }
 
 export { useBodyScrollLock };
